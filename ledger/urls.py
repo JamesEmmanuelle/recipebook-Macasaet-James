ea621d3f5021845a0eb9a99b5e208a15ae8ece5b
JamesEmmanuelle <157690400+JamesEmmanuelle@users.noreply.github.com>
2024-02-16 00:00:10 +0800

    Update urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import recipe_list, recipe_1, recipe_2
+from .views import recipe_list, recipe1, recipe2
 
 urlpatterns = [
     path('recipes/list', recipe_list, name='recipe_list'),
-    path('recipe/1', recipe_1, name='recipe_1'),
-    path('recipe/2', recipe_2, name='recipe_2')
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2')
 ]
 
 app_name = 'recipes'
