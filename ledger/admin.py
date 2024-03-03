4d3df85c0bf988655d055ace20e96bbba016f30b
jamesmacasaet <james.emmanuelle.m@gmail.com>
2024-03-03 15:09:59 +0800

    first lab2 commit

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
 # Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+class RecipeIngredientInline(admin.TabularInline):  
+    model = RecipeIngredient
+    extra = 1  
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity')
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    inlines = [RecipeIngredientInline]
