d71000ca2f3d726993b61ed6f7fb47de7b08b995
jamesmacasaet <james.emmanuelle.m@gmail.com>
2024-03-15 15:52:14 +0800

    fixed urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.custom_login, name='login'),
-    path('logout/', views.custom_logout, name='logout'),
-    path('recipe/<int:pk>/', views.recipe_detail, name='recipe-detail'),
+    path('/login/', views.custom_login, name='login'),
+    path('/logout/', views.custom_logout, name='logout'),
+    path('/recipe/<int:pk>/', views.recipe_detail, name='recipe-detail'),
 ]
--- ledger/urls.py
+++ ledger/urls.py
@@ -7,13 +7,3 @@ urlpatterns = [
     path('recipes/list/', RecipeListView.as_view(), name='recipe-list'),
     path('recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
-
-
-
-
-# urlpatterns = [
-#     path('recipes/list/', recipe_list, name='recipe-list'),
-#     path('recipes/<int:pk>/', recipe_detail, name='recipe-detail'),
-#     path('recipe/1/', recipe1, name='recipe1'),
-#     path('recipe/2/', recipe2, name='recipe2'),
-# ]
